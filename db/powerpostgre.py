e6c5fb92a48dcbf2dfef1019a4f634d2c4fa8e3c
Talgin <talgat90.07@gmail.com>
2023-01-02 13:03:13 +0600

    changed sql statements

--- db/powerpostgre.py
+++ db/powerpostgre.py
@@ -36,19 +36,23 @@ class PowerPost:
         return blob
 
 
-    def search_from_face_db(self, feature, threshold):
+    def search_from_face_db(self, vector, threshold):
         # connect to the PostgresQL database
         # FLAGS.psql_server, FLAGS.psql_server_port, FLAGS.psql_db, FLAGS.psql_user, FLAGS.psql_user_pass
         conn = psycopg2.connect(host=self.host, port=self.port, dbname=self.dbname, user=self.user,
                                 password=self.pwd)
         # create a new cursor object
         cur = conn.cursor()
-        select_query = '''SELECT fr.faces.unique_id, fr.faces.vector,
-                      (CUBE(array[{vector}]) <-> fr.faces.vector) as distance
+        # convert vector to SQL-readable text
+        vector_str = AsIs('CUBE(ARRAY[' + str(vector.tolist()).strip('[|]') + '])')
+        # SQL query
+        select_query = """SELECT fr.faces.unique_id, fr.faces.vector,
+                      %(vector)s <-> fr.faces.vector) as distance
                       FROM fr.faces
-                      ORDER BY (CUBE(array[{vector}]) <-> vector)
-                      ASC LIMIT 10'''.format(vector=','.join(str(s) for s in feature),)
-        cur.execute(select_query)
+                      ORDER BY %(vector)s <-> vector)
+                      ASC LIMIT 10"""
+        # execute statement
+        cur.execute(select_query, {'vector': vector_str})
         result = cur.fetchall()
         cur.close()
         distance = float(threshold)
@@ -57,7 +61,7 @@ class PowerPost:
         for row in result:
             vec = np.fromstring(row[1][1:-1], dtype=float, sep=',')
             try:
-                dist = np.dot(vec,feature)
+                dist = np.dot(vec,vector)
                 if dist > distance:
                     idx = row[0]
             except Exception as error:
@@ -67,20 +71,23 @@ class PowerPost:
             return idx, dist*100
         else:
             return idx, dist
-    
 
-    def one_to_one(self, feature, person_id, threshold):
+
+    def one_to_one(self, vector, person_id, threshold):
         # connect to the PostgresQL database
-        # FLAGS.psql_server, FLAGS.psql_server_port, FLAGS.psql_db, FLAGS.psql_user, FLAGS.psql_user_pass
         conn = psycopg2.connect(host=self.host, port=self.port, dbname=self.dbname, user=self.user,
                                 password=self.pwd)
         # create a new cursor object
         cur = conn.cursor()
-        select_query = '''SELECT fr.faces.unique_id, fr.faces.vector,
-                      (CUBE(array[{vector}]) <-> fr.faces.vector) as distance
+        # convert vector to SQL-readable text
+        vector_str = AsIs('CUBE(ARRAY[' + str(vector.tolist()).strip('[|]') + '])')
+        # select statement
+        select_query = """SELECT fr.faces.unique_id, fr.faces.vector,
+                      %(vector)s <-> fr.faces.vector) as distance
                       FROM fr.faces
-                      WHERE unique_id={unique_id}'''.format(vector=','.join(str(s) for s in feature), unique_id=person_id)
-        cur.execute(select_query)
+                      WHERE unique_id=%(uid)s"""
+        # execute statement
+        cur.execute(select_query, {'vector': vector_str, 'uid': person_id})
         result = cur.fetchall()
         cur.close()
         distance = float(threshold)
@@ -88,7 +95,7 @@ class PowerPost:
         dist = None
         for row in result:
             vec = np.fromstring(row[1][1:-1], dtype=float, sep=',')
-            dist = np.dot(vec,feature)
+            dist = np.dot(vec,vector)
             if dist > distance:
                 idx = row[0]
         if idx is not None:
@@ -96,29 +103,17 @@ class PowerPost:
         else:
             return idx, dist
 
-    
+
     def insert_new_person(self, unique_id, vector, person_name, person_surname, person_secondname, create_time, group_id, person_iin):
-        #try:
-        # connect to the PostgresQL database
-        conn = psycopg2.connect(host = self.host, port=self.port, dbname=self.dbname, user=self.user, password=self.pwd)
-        # create a new cursor object
-        cur = conn.cursor()
-        # query to be executed
-        # current_date
-        # insert_query = 
-        # query = sql.SQL(insert_query).format(
-        #                 unique_id=sql.Identifier(unique_id),
-        #                 vector_array=sql.Composable(','.join(str(s) for s in vector)),
-        #                 person_name=sql.Literal(person_name),
-        #                 person_surname=sql.Literal(person_surname),
-        #                 person_secondname=sql.Literal(person_secondname),
-        #                 insert_date=sql.Literal(create_time),
-        #                 group_id=sql.Literal(group_id),
-        #                 person_iin=sql.Literal(person_iin))
-        # execute the INSERT statement
-        # vector_str = ','.join(str(s) for s in vector)
-        vector_str = AsIs('cube(ARRAY[' + str(vector.tolist()).strip('[|]') + '])')
-        cur.execute("""
+        try:
+            # connect to the PostgresQL database
+            conn = psycopg2.connect(host = self.host, port=self.port, dbname=self.dbname, user=self.user, password=self.pwd)
+            # create a new cursor object
+            cur = conn.cursor()
+            # convert vector to SQL-readable text
+            vector_str = AsIs('CUBE(ARRAY[' + str(vector.tolist()).strip('[|]') + '])')
+            # execute the INSERT statement
+            cur.execute("""
                         WITH 
                             faces_tbl AS
                             (INSERT INTO fr.faces (unique_id, vector) VALUES (%(uid)s, %(vector)s) RETURNING unique_id),
@@ -130,20 +125,19 @@ class PowerPost:
                         """, {'uid': unique_id, 'vector': vector_str, 
                                 'p_name': person_name, 'p_surname': person_surname, 
                                 'p_sname': person_secondname, 'c_time': create_time, 
-                                'g_id': group_id, 'p_iin': person_iin,})
-        # commit the changes to the database
-        conn.commit()
-        # close the communication with the PostgresQL database
-        cur.close()
-    
-        # except Exception as error:
-        #     print('Error: ' + str(error))
-        #     return False
-        # finally:
-        #     if conn is not None:
-        #         conn.close()
+                                'g_id': group_id, 'p_iin': person_iin})
+            # commit the changes to the database
+            conn.commit()
+            # close the communication with the PostgresQL database
+            cur.close()
+        except Exception as error:
+            print('Error: ' + str(error))
+            return False
+        finally:
+            if conn is not None:
+                conn.close()
         return True
-        
+
 
     def insert_into_faces(self, unique_id, vector):
         try:
@@ -151,8 +145,10 @@ class PowerPost:
             conn = psycopg2.connect(host = self.host, port=self.port, dbname=self.dbname, user=self.user, password=self.pwd)
             # create a new cursor object
             cur = conn.cursor()
+            # convert vector to SQL-readable text
+            vector_str = AsIs('CUBE(ARRAY[' + str(vector.tolist()).strip('[|]') + '])')
             # execute the INSERT statement
-            cur.execute("INSERT INTO fr.faces(unique_id, vector) VALUES ('{}', CUBE(array[{}]))".format(unique_id, ','.join(str(s) for s in vector), ))
+            cur.execute("INSERT INTO fr.faces(unique_id, vector) VALUES (%(uid)s, %(vector)s)", {'uid': unique_id, 'vector': vector_str})
             # commit the changes to the database
             conn.commit()
             # close the communication with the PostgresQL database
@@ -173,7 +169,7 @@ class PowerPost:
             # create a new cursor object
             cur = conn.cursor()
             # execute the INSERT statement
-            cur.execute("DELETE FROM fr.faces WHERE unique_id = {}".format(unique_id))
+            cur.execute("DELETE FROM fr.faces WHERE unique_id = %(uid)s", {'uid': unique_id})
             # commit the changes to the database
             conn.commit()
             # close the communication with the PostgresQL database
@@ -185,34 +181,34 @@ class PowerPost:
             if conn is not None:
                 conn.close()
         return True
-    
-    # unique_id, person_name, person_surname, person_secondname, create_time, group_id
-    def insert_into_persons(self, unique_id, person_name, person_surname, person_secondname, create_time, group_id):
+
+
+    def insert_into_persons(self, unique_id, person_name, person_surname, person_secondname, create_time, group_id, person_iin):
         print(unique_id, person_name, person_surname, person_secondname, group_id)
         try:
             # connect to the PostgresQL database
             conn = psycopg2.connect(host=self.host, port=self.port, dbname=self.dbname, user=self.user, password=self.pwd)
             # create a new cursor object
             cur = conn.cursor()
+            # query
+            insert_query = """INSERT INTO
+                                fr.persons(
+                                unique_id,
+                                person_name,
+                                person_surname,
+                                person_secondname,
+                                create_time,
+                                group_id) VALUES (
+                                %(uid)s,%(p_name)s,%(p_surname)s,%(p_sname)s,%(c_time)s,%(g_id)s,%(p_iin)s
+                                )"""
             # execute the INSERT statement
-            insert_query = '''INSERT INTO
-                    fr.persons(
-                    unique_id,
-                    person_name,
-                    person_surname,
-                    person_secondname,
-                    create_time,
-                    group_id) VALUES (
-                    %s,%s,%s,%s,%s,%s
-                    )'''
-                    # .format(
-                    #    unique_id=unique_id,
-                    #    person_name=person_name,
-                    #    person_surname=person_surname,
-                    #    person_secondname=person_secondname,
-                    #    create_time=datetime.now(timezone.utc),
-                    #    group_id=str(group_id))
-            cur.execute(insert_query, (unique_id, person_name, person_surname, person_secondname, create_time, group_id))
+            cur.execute(insert_query, {'uid': unique_id, 
+                                        'p_name': person_name, 
+                                        'p_surname': person_surname, 
+                                        'p_sname': person_secondname, 
+                                        'c_time': create_time, 
+                                        'g_id': group_id, 
+                                        'p_iin': person_iin})
             # commit the changes to the database
             conn.commit()
             # close the communication with the PostgresQL database
@@ -233,7 +229,7 @@ class PowerPost:
             # create a new cursor object
             cur = conn.cursor()
             # execute the INSERT statement
-            cur.execute("DELETE FROM fr.persons WHERE unique_id = {}".format(unique_id))
+            cur.execute("DELETE FROM fr.persons WHERE unique_id = %s".format(unique_id))
             # commit the changes to the database
             conn.commit()
             # close the communication with the PostgresQL database
